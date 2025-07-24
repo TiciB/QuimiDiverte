f2a49899166253265895b79b73a0af5ea6198328
TiciB <tebezerra@gmail.com>
2025-07-24 03:48:40 -0300

    Adicionando QuimiDiverte como pasta comum

---
+++ chem.js
@@ -0,0 +1,135 @@
+export const quimicos = [
+  {
+    nome: "George Washington Carver",
+    perguntas: "Qual o nome do químico agrícola e agrônomo dos EUA, nascido escravo que se tornou diretor de pesquisa agrícola no Instituto Tuskegee no Alabama?",
+    alternativas: [
+      { texto: "John Dalton", correta: false },
+      { texto: "Linus Pauling", correta: false },
+      { texto: "Dmitri Mendeleev", correta: false },
+      { texto: "George Washington Carver", correta: true }
+    ],
+    foto: "./img/George-W-carver.jpg",
+    biografia: "Era um químico agrícola que aplicou conhecimentos químicos para desenvolver novos produtos a partir de culturas como amendoim, batata-doce e soja. Criou 500 corantes durante a Segunda Guerra Mundial, ajudando a economia agrícola do Sul dos EUA."
+  },
+  {
+    nome: "Alice Ball",
+    perguntas: "Quem foi a química responsável pela identificação e alteração de ácidos graxos presentes no óleo de Chaulmoogra utilizado no tratamento da hanseníase?",
+    alternativas: [
+      { texto: "Alice Ball", correta: true },
+      { texto: "Dorothy Crowfoot Hodgkin", correta: false },
+      { texto: "Marie Curie", correta: false },
+      { texto: "Rosalind Franklin", correta: false }
+    ],
+    foto: "./img/Alice_Augusta_Ball.jpg",
+    biografia: "Isolou os ácidos graxos do óleo de chaulmoogra e os converteu em ésteres de etila, aumentando sua absorção e eficácia contra hanseníase. Foi a primeira mulher negra a obter um mestrado em Química pela Universidade do Havaí."
+  },
+  {
+    nome: "Marie Maynard Daly",
+    perguntas: "Quem foi a primeira mulher negra a obter um doutorado em química nos EUA e ajudou a desvendar os mistérios do núcleo celular?",
+    alternativas: [
+      { texto: "Marie Curie", correta: false },
+      { texto: "Rachel Carson", correta: false },
+      { texto: "Marie Maynard Daly", correta: true },
+      { texto: "Gertrude B. Elion", correta: false }
+    ],
+    foto: "./img/marie_maynard_daly_lab.jpg",
+    biografia: "Primeira mulher negra a obter doutorado em Química nos EUA. Suas pesquisas ajudaram na compreensão das histonas e da relação entre pressão alta, colesterol e doenças cardíacas."
+  },
+  {
+    nome: "Bettye Washington Greene",
+    perguntas: "Quem foi a química negra a trabalhar na Dow química aprimorando o látex?",
+    alternativas: [
+      { texto: "Frances Arnold", correta: false },
+      { texto: "Alice Augusta Ball", correta: false },
+      { texto: "Bettye Washington Greene", correta: true },
+      { texto: "Irène Joliot-Curie", correta: false }
+    ],
+    foto: "./img/Bettye_Washington_Greene_at_work.tif.png",
+    biografia: "Primeira mulher negra na Dow Chemical, onde desenvolveu coloides e melhorou o látex. Detinha diversas patentes relacionadas a adesivos e revestimentos à base de látex."
+  },
+  {
+    nome: "Samuel P. Massie",
+    perguntas: "Qual químico negro participou do projeto Manhattan para a construção da bomba atômica durante a segunda guerra mundial?",
+    alternativas: [
+      { texto: "J. Robert Oppenheimer", correta: false },
+      { texto: "Neil deGrasse Tyson", correta: false },
+      { texto: "Linus Pauling", correta: false },
+      { texto: "Samuel P. Massie", correta: true }
+    ],
+    foto: "./img/Samuel_P_Massie_in_lab.jpg",
+    biografia: "Participou do Projeto Manhattan, trabalhou com isótopos de urânio. Também descobriu compostos antimicrobianos e foi o primeiro negro a lecionar química na Academia Naval dos EUA."
+  },
+  {
+    nome: "James Ellis Lu Valle",
+    perguntas: "Qual foi o químico que participou das olimpíadas de Berlim em 1936 e ganhou 4 medalhas de ouro?",
+    alternativas: [
+      { texto: "James Ellis Lu Valle", correta: true },    
+      { texto: "Usain Bolt", correta: false },
+      { texto: "Carl Lewis", correta: false },
+      { texto: "Michael Phelps", correta: false }
+      
+    ],
+    foto: "./img/james_valle.jpg",
+    biografia: "Ganhou bronze nos 400m nas Olimpíadas de 1936 e foi um importante químico, tendo estudado com Linus Pauling. Trabalhou em empresas como Eastman Kodak e Smith-Corona Marchant."
+  },
+  {
+    nome: "Robert Henry Lawrence Jr.",
+    perguntas: "Quem foi o químico que se tornou o primeiro astronauta afro-americano?",
+    alternativas: [
+      { texto: "Buzz Aldrin", correta: false },
+      { texto: "Neil Armstrong", correta: false },
+      { texto: "Robert Henry Lawrence Jr.", correta: true },
+      { texto: "Yuri Gagarin", correta: false }
+    ],
+    foto: "./img/robert-henry-lawrence-jr.png",
+    biografia: "Primeiro astronauta afro-americano. Doutor em físico-química, morreu em um acidente de voo em 1967. Deixou um legado educacional com bolsas e escolas em seu nome."
+  },
+  {
+    nome: "Angie Turner King",
+    perguntas: "Quem foi a química que, durante a segunda guerra mundial, lecionou a matéria química para os soldados?",
+    alternativas: [
+      { texto: "Barbara McClintock", correta: false },
+      { texto: "Emmanuelle Charpentier", correta: false },
+      { texto: "Jennifer Doudna", correta: false },
+      { texto: "Angie Turner King", correta: true }
+    ],
+    foto: "./img/Photo_of_Angie_Turner_King.jpg",
+    biografia: "Lecionou química e matemática. Doutora em educação. Ensinou soldados durante a Segunda Guerra Mundial em programa da Universidade Estadual da Virgínia Ocidental."
+  },
+  {
+    nome: "Percy Lavon Julian",
+    perguntas: "Quem foi o químico que sintetizou em 11 etapas a fisostigmina utilizada no tratamento do glaucoma?",
+    alternativas: [
+      { texto: "Alexander Fleming", correta: false },
+      { texto: "Louis Pasteur", correta: false },
+      { texto: "Percy Lavon Julian", correta: true },
+      { texto: "Robert Koch", correta: false }
+    ],
+    foto: "./img/percy_julian.jpg",
+    biografia: "Químico que sintetizou a fisostigmina, um tratamento para glaucoma. Superou barreiras raciais e foi pioneiro em diversas áreas da química orgânica nos EUA."
+  },
+  {
+    nome: "Alma Levant Hayden",
+    perguntas: "Qual o nome da química americana que se tornou uma das primeiras cientistas negras a ingressar no FDA em 1963?",
+    alternativas: [
+      { texto: "Alma Levant Hayden", correta: true },
+      { texto: "Ada Yonath", correta: false },
+      { texto: "Rosalind Franklin", correta: false },
+      { texto: "Gerty Cori", correta: false }
+    ],
+    foto: "./img/Alma-Hayden.jpg",
+    biografia: "Química americana que ganhou atenção nacional em 1963 ao expor o Krebiozen. Trabalhou na FDA e pesquisou a identificação de substâncias como esteróides e barbitúricos usando cromatografia em papel."
+  },
+  {
+    nome: "Alma Levant Hayden",
+    perguntas: "Qual a técnica utilizada por Alma Levant nos estudos de determinação de esteróides adrenocorticais, barbitúricos e sulfonamidas?",
+    alternativas: [
+      { texto: "Espectroscopia de infravermelho", correta: false },
+      { texto: "Microscopia eletrônica", correta: false },
+      { texto: "Espectrometria de massa", correta: false },
+      { texto: "Cromatografia em papel", correta: true }
+    ],
+    foto: "./img/Alma-Hayden.jpg",
+    biografia: "Química americana que ganhou atenção nacional em 1963 ao expor o Krebiozen. Trabalhou na FDA e pesquisou a identificação de substâncias como esteróides e barbitúricos usando cromatografia em papel."
+  }
+];
---
+++ script.js
@@ -0,0 +1,153 @@
+import { quimicos } from "./chem.js";
+
+const startButton = document.querySelector(".start-game");
+const questionContainer = document.querySelector(".questions-container");
+const answersContainer = document.querySelector(".answers-container");
+const questionText = document.querySelector(".question-text");
+const nextQuestionButton = document.querySelector(".next-question");
+const playAgainButton = document.querySelector(".play-again");
+const saibaMaisButton = document.querySelector(".biblio");
+const secundario = document.querySelector(".app2");
+const nameEl = document.getElementById("name");
+const imageEl = document.getElementById("image");
+const bioEl = document.getElementById("bio");
+const nextChem = document.querySelector(".next-chem");
+const principal = document.querySelector(".app");
+const voltaInicio = document.querySelector(".again-button");
+
+startButton.addEventListener("click", startGame)
+nextQuestionButton.addEventListener("click", displayNextQuestion);
+playAgainButton.addEventListener("click", () => {
+  window.location.reload();
+});
+saibaMaisButton.addEventListener("click", () => {
+  principal.classList.add("hide"); 
+  secundario.classList.remove("hide");
+  showChemi(currentChemiIndex);
+})
+nextChem.addEventListener("click", () => {
+  currentChemiIndex++;
+  
+  showChemi(currentChemiIndex);
+});
+voltaInicio.addEventListener("click", () => {
+  window.location.reload();
+});
+
+let currentQuestionIndex = 0;
+let totalCorrectAnswers = 0;
+let currentChemiIndex = 0;
+
+
+function startGame() {
+    startButton.classList.add("hide");
+    questionContainer.classList.remove("hide");
+    displayNextQuestion();
+}
+
+function displayNextQuestion(){
+    resetState();
+
+    if(quimicos.length === currentQuestionIndex){
+        return finishGame();
+    }
+
+    questionText.innerText = quimicos[currentQuestionIndex].perguntas;
+    quimicos[currentQuestionIndex].alternativas.forEach(alternativa => {
+        const newAnswer = document.createElement("button");
+        newAnswer.innerText = alternativa.texto;
+        newAnswer.classList.add("btn", "answer");
+        newAnswer.textContent = alternativa.texto;
+        answersContainer.appendChild(newAnswer);
+
+        if (alternativa.correta) {
+            newAnswer.dataset.correct = alternativa.correta;
+        }
+
+        newAnswer.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    while (answersContainer.firstChild) {
+        answersContainer.removeChild(answersContainer.firstChild);
+    }
+
+    nextQuestionButton.classList.add("hide");
+}
+
+function selectAnswer(event){
+    const selectButton = event.target;
+    const isCorrect = selectButton.dataset.correct === "true";
+
+    if (isCorrect) {
+        selectButton.classList.add("correct");
+        totalCorrectAnswers++;
+    } else {
+        selectButton.classList.add("incorrect");
+    }
+    
+    document.querySelectorAll(".answer").forEach(button => {
+        button.disabled = true;
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        } else {
+            button.classList.add("incorrect");
+        }
+    });
+
+    currentQuestionIndex++;
+    if (currentQuestionIndex === quimicos.length) {
+        finishGame();
+    } else {
+        nextQuestionButton.classList.remove("hide");
+    }
+}
+
+function finishGame() {
+  const totalQuestions = quimicos.length;
+  const performance = Math.round((totalCorrectAnswers / totalQuestions) * 100);
+
+  let message = " ";
+
+  switch (true) {
+    case performance >= 90:
+      message = "Que lindo, cara!";
+      break;
+    case performance >= 60:
+      message = "Vamo tá estudando, hein?";
+      break;
+    case performance >= 40:
+      message = "Eu só digo uma coisa, eu nao digo é nada!";
+      break;
+    default:
+      message = "Aperta aí esse botão de saiba mais e vai estudar!";
+  }
+
+  questionContainer.innerHTML = 
+  `
+    <p style="font-family: 'Exo 2', sans-serif; font-size: 20px; color: #001e4d; text-align: center;">
+    Acertos: ${totalCorrectAnswers} de ${totalQuestions} perguntas. <br/>
+    <span> ${message}</span>
+    </p>
+
+  `
+  playAgainButton.classList.remove("hide");
+  saibaMaisButton.classList.remove("hide")
+
+}
+
+function showChemi(index) {   
+  const chemi = quimicos[index];
+  nameEl.textContent = chemi.nome;
+  imageEl.src = chemi.foto;
+  imageEl.alt = `Retrato de ${chemi.nome}`;
+  bioEl.textContent = chemi.biografia;
+
+ /* if (index === quimicos.length - 1) {
+    nextBtn.textContent = "Voltar ao início";
+  } else {
+    nextBtn.textContent = "Próxima Pergunta";
+  }*/
+}
+
